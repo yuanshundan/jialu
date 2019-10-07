778647e35a9297f2449a1b53c094ff67c4f57f18
yangjialu <573669898@qq.com>
2019-10-08 04:47:17 +0800

    commit

--- learn_jiekou/headers_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://api.yonyoucloud.com/apis/pte/New_Issue_Bond_Information/New_Issue_Bond_Information?size=2'
-headers = {'apicode': '0ec6701427a7418d860de8968d137114'}  # 添加请求头域
-
-# post请求
-resp = requests.post(url, headers=headers)
-print(resp.text)
--- learn_jiekou/tianqi.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://www.tianqiapi.com/api/'
-param = {'appid': 'nalan358', 'appsecret': '123456789', 'version': 'v1', 'city': '宜春'}  # city可选参数
-
-# get请求
-resp = requests.get(url, params=param)
-print(resp.text)
